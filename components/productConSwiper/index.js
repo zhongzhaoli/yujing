61ca4cd6919002795a94604b2c0a666973a4494c
钟兆立 <7458806+zhong_zhao_li@user.noreply.gitee.com>
2020-07-04 14:51:20 +0800

    first commit

---
+++ components/productConSwiper/index.js
@@ -0,0 +1,31 @@
+var app = getApp();
+Component({
+  properties: {
+    imgUrls:{
+      type:Object,
+      value:[]
+    },
+    videoline: 
+      {
+        type:String,
+        value:""
+      }
+  },
+  data: {
+    indicatorDots: true,
+    circular: true,
+    autoplay: false,
+    interval: 3000,
+    duration: 500,
+    currents: "1"
+  },
+  attached:function(){
+  },
+  methods: {
+    change: function (e) {
+      this.setData({
+        currents: e.detail.current + 1
+      })
+    }
+  }
+})
